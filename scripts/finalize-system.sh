7addc7527b6b58754cad26712c31d4b300761071
Yara <156561237+yaradotexe@users.noreply.github.com>
2026-05-22 11:17:52 +0200

    Add files via upload

---
+++ scripts/finalize-system.sh
@@ -0,0 +1,27 @@
+echo "Cleaning up Bazzite default packages..."
+
+REMOVE_PKGS=(
+    "bazzite-portal"
+    "steam"
+    "steam-devices"
+    "lutris"
+    "heroic-games-launcher-bin"
+)
+
+for pkg in "${REMOVE_PKGS[@]}"; do
+    if rpm -q "$pkg" &> /dev/null; then
+        rpm-ostree override remove "$pkg"
+    fi
+done
+
+
+systemctl enable icarus-user-rename.service
+
+mkdir -p /etc/xdg
+cat <<EOF > /etc/xdg/ksplashrc
+[KSplash]
+Theme=None
+Engine=None
+EOF
+
+echo "System cleanup and optimizations complete."
